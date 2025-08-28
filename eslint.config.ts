d194a8f101d3c469d42bfe2e70d6c4250bb323cd
Jon Staab <shtaab@gmail.com>
2025-08-28 16:23:44 -0700

    Implement very basic message handling

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,29 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier"
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {...globals.browser, ...globals.node}
+    }
+  },
   tseslint.configs.recommended,
+  prettier,
+  {
+    ignores: ["build/", ".svelte-kit/", "dist/"],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {args: "none", destructuredArrayIgnorePattern: "^_d?$", caughtErrors: "none"},
+      ],
+    }
+  }
 ]);
